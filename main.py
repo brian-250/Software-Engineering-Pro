959ffec1c99eeb9f698bbfa8b2e8b0df22ea9f21
csuftitan <csuftitan@MacBook-Air-179.local>
2023-09-22 10:33:36 -0700

    Add CustomChatGPT function

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ openai.api_key = ""
 # initial element is a dictionary in a the messages list
 # initial element communicates to the model what type of responses it should provide
 messages = [{"role": "system", "content": "You are a software engineering expert."}]
+
+def CustomChatGPT(user_input):
+
+    # Messages from the user are added to the messages list as a dictionary element
+    messages.append({"role": "user", "content": user_input})
+
+    # A response is generated from the model by calling the openAI ChatCompletion endpoint
+    response = openai.ChatCompletion.create(
+        # The model being used to generate a response is the gpt-3.5-turbo
+        model = "gpt-3.5-turbo",
+        # The messages list is added as a parameter so the model can generate a response based on all the conversation history
+        messages = messages
+    )
+    # The generated response from the model is stored in ChatGPT_reply
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    # The model's generated reponse is added to the messages list as a dictionary element
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    # The model's response stored in ChatGPT_reply is returned to the user
+    return ChatGPT_reply
