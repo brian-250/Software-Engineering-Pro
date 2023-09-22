7da6e4b0d5ee2eb59f9871c63f83a53b521a12bf
csuftitan <csuftitan@MacBook-Air-179.local>
2023-09-22 10:00:39 -0700

    Initialized messages list

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import gradio
 
 # Insert your openAI api key within the quotation marks
 openai.api_key = ""
+
+# messages list is defined with an initial element
+# initial element is a dictionary in a the messages list
+# initial element communicates to the model what type of responses it should provide
+messages = [{"role": "system", "content": "You are a software engineering expert."}]
