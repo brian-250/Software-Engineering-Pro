826c5f65ab61537133539a7916743b86234ef502
csuftitan <csuftitan@MacBook-Air-179.local>
2023-09-22 10:53:41 -0700

    Add demo object to generate a user interface

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import gradio
 openai.api_key = ""
 
 # messages list is defined with an initial element
-# initial element is a dictionary in a the messages list
+# initial element is a dictionary in the messages list
 # initial element communicates to the model what type of responses it should provide
 messages = [{"role": "system", "content": "You are a software engineering expert."}]
 
@@ -27,5 +27,8 @@ def CustomChatGPT(user_input):
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     # The model's generated reponse is added to the messages list as a dictionary element
     messages.append({"role": "assistant", "content": ChatGPT_reply})
-    # The model's response stored in ChatGPT_reply is returned to the user
+    # The model's response that is stored in ChatGPT_reply is returned to the user
     return ChatGPT_reply
+
+# demo object is created from the gradio.Interface class with parameters to generate a user interface
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Software Engineering Pro")
